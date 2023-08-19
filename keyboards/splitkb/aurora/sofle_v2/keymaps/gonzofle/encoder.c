8655a4d798a1517490f56b0926415895f5d15e56
Gonzalo Ricalde <gonzuki@gmail.com>
2023-08-19 14:22:37 +0100

    Changed encoder to scrub through undo/redo
    Removed a hack i had put in to account for encoder resolution in favour of fixing it through the correct mechanism in config.h

--- keyboards/splitkb/aurora/sofle_v2/keymaps/gonzofle/encoder.c
+++ keyboards/splitkb/aurora/sofle_v2/keymaps/gonzofle/encoder.c
@@ -2,34 +2,24 @@
   * Copyright 2020 Josef Adamcik
   */
 
-//Setting up what encoder rotation does. If your encoder can be pressed as a button, that function can be set in Via.
-
+#include "keycodes.h"
 #ifdef ENCODER_ENABLE
-
-
-//there's probably a correct way of detecting the steps on the encoder but for now i just have this flip flop hack
-static bool flipFlop = false;
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        flipFlop=!flipFlop;
-        if(flipFlop)
-        {
-            if (clockwise) {
-                tap_code(KC_MPRV);
-            } else {
-                tap_code(KC_MNXT);
-            }
+        if (clockwise) {
+            tap_code(KC_MNXT);
+        } else {
+            tap_code(KC_MPRV);
         }
     } else if (index == 1) {
-        //second encoder is reversed, is it because this is a split keyboard?
         if (!clockwise) {
-            tap_code16(RALT(KC_TAB));
+            tap_code16(C(KC_Z));
         } else {
-            tap_code16(RALT(S(KC_TAB)));
+            tap_code16(C(KC_Y));
         }
     }
-    return true;
+    return false;
 }
 
 #endif
