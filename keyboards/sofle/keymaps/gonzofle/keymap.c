78c4e09d87f59f0f448ed5bc4aae42bfaa8d6995
Gonzalo Ricalde <gonzuki@gmail.com>
2024-01-05 17:40:00 +0000

    removed declare and encoder files that are no longer necessary for aurora sofle, removed wonky esc functionality for gonzofle v1

--- keyboards/sofle/keymaps/gonzofle/keymap.c
+++ keyboards/sofle/keymaps/gonzofle/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT(
   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,
-  G_BESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+  KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
   KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   G_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,         KC_GUITB,    KC_N,    KC_M, KC_COMM, KC_DOT , KC_SLSH,  G_RSFT,
                      KC_GAME, KC_LCTL, G_LALT, G_LOWER, G_CENT,          G_ASPC, G_RAISE, G_RALT, G_RCTL, KC_RGUI
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                             KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,           KC_GUITB,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-                 KC_GAME,KC_LALT,KC_LCTL, KC_SPC, KC_ENT,                   KC_SPC,  KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI
+                 KC_GAME,KC_LALT,KC_LCTL, KC_SPC, G_CENT,                   KC_SPC,  KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI
 ),
 
 /*
